2b027faf9cbe0e4aac1484d155abcf15a1ff48be
ambeech <beecheraidan1@gmail.com>
2024-11-26 15:14:27 -0500

    Modified TISE window.

--- tisemainwindow.cpp
+++ tisemainwindow.cpp
@@ -6,9 +6,24 @@ TISEMainWindow::TISEMainWindow(QWidget *parent)
     , ui(new Ui::TISEMainWindow)
 {
     ui->setupUi(this);
+    ui->tabs1d->setCurrentIndex(0);
+    ui->tabs2d->setCurrentIndex(0);
 }
 
 TISEMainWindow::~TISEMainWindow()
 {
     delete ui;
 }
+
+
+void TISEMainWindow::on_tise1DTabInfo_clicked()
+{
+    QMessageBox::about(this, "Tab Descriptions", "Plot Settings: Adjust the appearance and axis ranges of the plot window.\n\nPotential Energy: Configure the desired potential energy function and boundary conditions.\n\nSolution Parameters: Adjust the accuracy of the solution, as well as particle mass and the scale of Planck's constant.\n\nDisplay: Choose what variables should appear on the plot.\n\nManage Solutions: Show and hide previously found solutions, and save desired solutions to be viewed and manipulated in the TDSE module.");
+}
+
+
+void TISEMainWindow::on_pushButton_2_clicked()
+{
+    QMessageBox::about(this, "Tab Descriptions", "Plot Settings: Adjust the appearance and axis ranges of the plot window.\n\nPotential Energy: Configure the desired potential energy function and boundary conditions. Specify a spin-dependency if desired.\n\nSolution Parameters: Adjust the accuracy of the solution, as well as particle mass and the scale of Planck's constant.\n\nDisplay: Choose what variables should appear on the plot.\n\nManage Solutions: Show and hide previously found solutions, and save desired solutions to be viewed and manipulated in the TDSE module.\n\nSpin Parameters: Configure the spin components of the wavefunction, if desired.");
+}
+
--- tisemainwindow.h
+++ tisemainwindow.h
@@ -15,6 +15,11 @@ public:
     explicit TISEMainWindow(QWidget *parent = nullptr);
     ~TISEMainWindow();
 
+private slots:
+    void on_tise1DTabInfo_clicked();
+
+    void on_pushButton_2_clicked();
+
 private:
     Ui::TISEMainWindow *ui;
 };
